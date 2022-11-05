178cd18b33daa909340b10dece70a62203b88861
amolmane7471 <maneamol3233@gmail.com>
2022-11-05 22:21:57 +0530

    [Amol] : added section2 UC2 introducing service layer in emppayroll app

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/services/EmployeePayrollService.java
@@ -0,0 +1,43 @@
+package com.bridgelabz.employeepayrollapp.services;
+
+import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+@Service
+public class EmployeePayrollService implements IEmployeePayrollService{
+    @Override
+    public List<EmployeePayrollData> getEmployeePayrollData() {
+        List<EmployeePayrollData> empDataList = new ArrayList<>();
+        empDataList.add(new EmployeePayrollData(1, new EmployeePayrollDTO("Rohit", 600000)));
+        return empDataList;
+    }
+
+    @Override
+    public EmployeePayrollData getEmployeePayrollDataById(int empId) {
+        EmployeePayrollData empData = null;
+        empData = new EmployeePayrollData(1, new EmployeePayrollDTO("Rohit", 600000));
+        return empData;
+    }
+
+    @Override
+    public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
+        EmployeePayrollData empData = null;
+        empData = new EmployeePayrollData(1, empPayrollDTO);
+        return empData;
+    }
+
+    @Override
+    public EmployeePayrollData updateEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
+        EmployeePayrollData empData = null;
+        empData = new EmployeePayrollData(1, empPayrollDTO);
+        return empData;
+    }
+
+    @Override
+    public void deleteEmployeePayrollData(int empId) {
+
+    }
+}
---
+++ src/main/java/com/bridgelabz/employeepayrollapp/services/IEmployeePayrollService.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.employeepayrollapp.services;
+
+import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
+
+import java.util.List;
+
+public interface IEmployeePayrollService {
+    List<EmployeePayrollData> getEmployeePayrollData();
+
+    EmployeePayrollData getEmployeePayrollDataById(int empId);
+
+    EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO);
+
+    EmployeePayrollData updateEmployeePayrollData(EmployeePayrollDTO empPayrollDTO);
+
+    void deleteEmployeePayrollData(int empId);
+}
