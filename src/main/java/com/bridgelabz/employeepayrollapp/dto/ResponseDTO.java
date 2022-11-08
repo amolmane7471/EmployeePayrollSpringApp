cc5a026f20bf689effe3f934094627426e24254b
amolmane7471 <maneamol3233@gmail.com>
2022-11-08 12:29:58 +0530

    [Amol] : added section3 UC2 use lombok library for logging

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/ResponseDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/ResponseDTO.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.employeepayrollapp.dto;
-
 import lombok.Data;
-
+/*
+ *@Data is equivalent to using @getters @setters @RequiredArgsConstructor @ToString
+ */
 public @Data class ResponseDTO {
     private String message;
     private Object data;
